63f504cb11fce585449d64551f403d9ab04bde49
Mekan Hydyrov <mekanhydyrov@gmail.com>
2022-05-12 01:04:48 +0300

    Add project files.

---
+++ SHOPAPP.WEBUI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SHOPAPP.WEBUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "home/index";
+        }
+    }
+}
